2170f1ab5e157b056396e2d206781bc7a0941495
Merugu Vaishnavi <vaishnavimerugu2005@gmail.com>
2025-12-02 11:20:17 +0530

    Commit2

--- LinkedList.java
+++ LinkedList.java
@@ -15,6 +15,18 @@ class LinkedList {
         newNode.next = head;
         head = newNode;
     }
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
     public void display() {
         Node current = head;
         while (current != null) {
@@ -31,7 +43,10 @@ public class LinkedListDemo {
         list.insertAtBeginning(10);
         list.insertAtBeginning(20);
         list.insertAtBeginning(30);
+        list.insertAtEnd(40);
+        list.insertAtEnd(50);
         System.out.println("Linked List after inserting at beginning:");
         list.display();
+        list.insertAtEnd(40);
     }
 }
